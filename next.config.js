4a72474c9dca79ad6ee7ce5be15402f709cbae3e
AMARAFK007 <amarjitpradhan007@gmail.com>
2025-05-18 17:44:58 +0530

    Add contact page with EmailJS integration and configuration files

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  output: 'export',
+  distDir: 'out',
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig; 
